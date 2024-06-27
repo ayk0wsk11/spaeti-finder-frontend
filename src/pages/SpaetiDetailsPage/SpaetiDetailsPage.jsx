817db4bbf4af8cf5952ab20a337560b4fb030959
Jonathan Senf <jonathansenf97@gmail.com>
2024-06-27 16:16:50 +0200

    detail page

--- src/pages/SpaetiDetailsPage/SpaetiDetailsPage.jsx
+++ src/pages/SpaetiDetailsPage/SpaetiDetailsPage.jsx
@@ -1,6 +1,32 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/auth.context";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../../config";
+
 const SpaetiDetailsPage = () => {
-  return (
-    <div>SpaetiDetailsPage</div>
-  )
-}
-export default SpaetiDetailsPage
+  const { currentUser } = useContext(AuthContext);
+  const [comment, setComment] = useState();
+  const [oneSpaeti, setOneSpaeti] = useState();
+
+  const { spaetiId } = useParams();
+  const nav = useNavigate();
+
+  useEffect(()=>{
+
+    const fetchData = async ()=>{
+
+      try {
+        const {data} = await axios.get(`${API_URL}/spaetis/${spaetiId}`)
+        setOneSpaeti(data.data)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData();
+  },[spaetiId])
+
+  return <div></div>;
+};
+export default SpaetiDetailsPage;
