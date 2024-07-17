abb761aaa1d850decc2ceb9b9cd347b947fdd528
Jonathan Senf <jonathansenf97@gmail.com>
2024-07-17 12:29:11 +0200

    added cluster to map

--- src/components/SpatiMap/SpatiMap.jsx
+++ src/components/SpatiMap/SpatiMap.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from "react";
+// LEAFLET IMPORTS
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import MarkerClusterGroup from "react-leaflet-cluster";
+
+// REST
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { API_URL } from "../../config";
 import "./SpatiMap.css";
 import { Link } from "react-router-dom";
 import Icon from "../../assets/Gollum.png";
-delete L.Icon.Default.prototype._getIconUrl;
 
+delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
     "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
@@ -68,23 +72,31 @@ const SpatiMap = () => {
   }, []);
 
   return (
-    <MapContainer center={[52.52, 13.405]} zoom={12} id="map">
-
+    <MapContainer center={[52.52, 13.405]} zoom={15} id="map">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
 
-      {spatis.map((spati, idx) => {
-        if (spati.approved && spati.lat && spati.lng)
-          return (
-            <Marker key={idx} position={[spati.lat, spati.lng]}>
-              <Popup>
-                <Link to={`/spaeti/details/${spati._id}`}>{spati.name}</Link>
-              </Popup>
-            </Marker>
-          );
-      })}
+      <MarkerClusterGroup
+        maxClusterRadius={150}
+        spiderfyOnMaxZoom={true}
+        chunkedLoading
+        showCoverageOnHover={true}
+        singleMarkerMode={true}
+      >
+        {spatis.map((spati, idx) => {
+          if (spati.approved && spati.lat && spati.lng)
+            return (
+              <Marker key={idx} position={[spati.lat, spati.lng]}>
+                <Popup>
+                  <Link to={`/spaeti/details/${spati._id}`}>{spati.name}</Link>
+                </Popup>
+              </Marker>
+            );
+        })}
+      </MarkerClusterGroup>
+
       {userLocation && (
         <Marker position={[userLocation.lat, userLocation.lng]} icon={userIcon}>
           <Popup>My precious</Popup>
